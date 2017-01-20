35dc8f5e661085e4f993651fc6b97702e3111de9
Matthew Heinsen Egan <sen.tieshar@gmail.com>
2017-01-20 18:01:46 +0800

    Change compiler to GCC.
    Because wxWidgets fails under Clang on msys2.

--- scripts/appveyor/build.sh
+++ scripts/appveyor/build.sh
@@ -1,5 +1,5 @@
 cd "/c/projects/seec_clang_build"
-"C:\\Program Files (x86)\\cmake\\bin\\cmake.exe" -G"MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_EH=true -DLLVM_ENABLE_PIC=true -DLLVM_ENABLE_RTTI=true -DLLVM_INCLUDE_TESTS=off -DCLANG_BUILD_TOOLS=off -DCMAKE_PROGRAM_PATH="C:/projects/deps/bin" -DPYTHON_EXECUTABLE="C:/msys64/mingw64/bin/python2.7.exe" -DENABLE_SHARED=off -DCMAKE_INSTALL_PREFIX="C:\\projects\\seec_clang_install" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ $APPVEYOR_BUILD_FOLDER
+"C:\\Program Files (x86)\\cmake\\bin\\cmake.exe" -G"MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_EH=true -DLLVM_ENABLE_PIC=true -DLLVM_ENABLE_RTTI=true -DLLVM_INCLUDE_TESTS=off -DCLANG_BUILD_TOOLS=off -DCMAKE_PROGRAM_PATH="C:/projects/deps/bin" -DPYTHON_EXECUTABLE="C:/msys64/mingw64/bin/python2.7.exe" -DENABLE_SHARED=off -DCMAKE_INSTALL_PREFIX="C:\\projects\\seec_clang_install" $APPVEYOR_BUILD_FOLDER
 make -j4
 make install
 cd $APPVEYOR_BUILD_FOLDER
