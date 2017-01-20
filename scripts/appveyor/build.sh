ece2b24e3b6e35f922c06ff87e6ae663c2a79e83
Matthew Heinsen Egan <sen.tieshar@gmail.com>
2017-01-20 14:36:26 +0800

    Appveyor building for SeeC-Clang
    (using external LLVM from llvm-with-seec-clang appveyor build artifact)

---
+++ scripts/appveyor/build.sh
@@ -0,0 +1,6 @@
+cd "/c/projects/seec_clang_build"
+"C:\\Program Files (x86)\\cmake\\bin\\cmake.exe" -G"MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_EH=true -DLLVM_ENABLE_PIC=true -DLLVM_ENABLE_RTTI=true -DLLVM_INCLUDE_TESTS=off -DCLANG_BUILD_TOOLS=off -DCMAKE_PROGRAM_PATH="C:/projects/deps/bin" -DPYTHON_EXECUTABLE="C:/msys64/mingw64/bin/python2.7.exe" -DENABLE_SHARED=off -DCMAKE_INSTALL_PREFIX="C:\\projects\\seec_clang_install" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ $APPVEYOR_BUILD_FOLDER
+make -j4
+make install
+cd $APPVEYOR_BUILD_FOLDER
+7z a seec_clang_install_3.9.0.zip "C:\\projects\\seec_clang_install\\*"
---
+++ scripts/appveyor/install.bat
@@ -0,0 +1,29 @@
+@echo off
+
+cd %APPVEYOR_BUILD_FOLDER%
+
+echo Compiler: %COMPILER%
+echo Platform: %PLATFORM%
+echo MSYS2 directory: %MSYS2_DIR%
+echo MSYS2 system: %MSYSTEM%
+
+REM Create a writeable TMPDIR
+mkdir %APPVEYOR_BUILD_FOLDER%\tmp
+set TMPDIR=%APPVEYOR_BUILD_FOLDER%\tmp
+
+IF %COMPILER%==msys2 (
+  @echo on
+  SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
+
+  pacman -S --noconfirm mingw-w64-x86_64-clang mingw-w64-x86_64-libc++
+
+  REM download and extract llvm build artifact
+  mkdir c:\projects\deps
+  cd c:\projects\deps
+  appveyor DownloadFile https://ci.appveyor.com/api/projects/mheinsen/llvm-with-seec-clang/artifacts/llvm_install_3.9.0.zip?branch=release_39_appveyor
+  7z x llvm_install_3.9.0.zip -y
+  dir c:\projects\deps
+
+  REM causes problems linking libclang:
+  del C:\msys64\usr\lib\libdl.a
+)
